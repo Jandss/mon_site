4d668fbc61e174f9418046da2e6672227be6091c
Jandss <104347607+Jandss@users.noreply.github.com>
2023-06-08 11:58:49 +0200

    Add files via upload

---
+++ qui_est_ce/tueur_dbd/script.js
@@ -0,0 +1,93 @@
+const c1 = document.querySelector(".c1");
+const c2 = document.querySelector(".c2");
+const c3 = document.querySelector(".c3");
+const c4 = document.querySelector(".c4");
+const c5 = document.querySelector(".c5");
+const c6 = document.querySelector(".c6");
+const c7 = document.querySelector(".c7");
+const c8 = document.querySelector(".c8");
+const c9 = document.querySelector(".c9");
+const c10 = document.querySelector(".c10");
+const c11 = document.querySelector(".c11");
+const c12 = document.querySelector(".c12");
+const c13 = document.querySelector(".c13");
+const c14 = document.querySelector(".c14");
+const c15 = document.querySelector(".c15");
+const c16 = document.querySelector(".c16");
+const c17 = document.querySelector(".c17");
+const c18 = document.querySelector(".c18");
+const c19 = document.querySelector(".c19");
+const c20 = document.querySelector(".c20");
+const c21 = document.querySelector(".c21");
+const c22 = document.querySelector(".c22");
+const c23 = document.querySelector(".c23");
+
+c1.addEventListener("click", () => {
+	c1.classList.toggle("gris");
+});
+c2.addEventListener("click", () => {
+	c2.classList.toggle("gris");
+});
+c3.addEventListener("click", () => {
+	c3.classList.toggle("gris");
+});
+c4.addEventListener("click", () => {
+	c4.classList.toggle("gris");
+});
+c5.addEventListener("click", () => {
+	c5.classList.toggle("gris");
+});
+c6.addEventListener("click", () => {
+	c6.classList.toggle("gris");
+});
+c7.addEventListener("click", () => {
+	c7.classList.toggle("gris");
+});
+c8.addEventListener("click", () => {
+	c8.classList.toggle("gris");
+});
+c9.addEventListener("click", () => {
+	c9.classList.toggle("gris");
+});
+c10.addEventListener("click", () => {
+	c10.classList.toggle("gris");
+});
+c11.addEventListener("click", () => {
+	c11.classList.toggle("gris");
+});
+c12.addEventListener("click", () => {
+	c12.classList.toggle("gris");
+});
+c13.addEventListener("click", () => {
+	c13.classList.toggle("gris");
+});
+c14.addEventListener("click", () => {
+	c14.classList.toggle("gris");
+});
+c15.addEventListener("click", () => {
+	c15.classList.toggle("gris");
+});
+c16.addEventListener("click", () => {
+	c16.classList.toggle("gris");
+});
+c17.addEventListener("click", () => {
+	c17.classList.toggle("gris");
+});
+c18.addEventListener("click", () => {
+	c18.classList.toggle("gris");
+});
+c19.addEventListener("click", () => {
+	c19.classList.toggle("gris");
+});
+c20.addEventListener("click", () => {
+	c20.classList.toggle("gris");
+});
+c21.addEventListener("click", () => {
+	c21.classList.toggle("gris");
+});
+c22.addEventListener("click", () => {
+	c22.classList.toggle("gris");
+});
+c23.addEventListener("click", () => {
+	c23.classList.toggle("gris");
+});
